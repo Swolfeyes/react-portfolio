96976b6b68a6a82c5a611e09ffc5ef9772c2286b
Stefania <s.occhilupo87@gmail.com>
2019-03-10 18:23:17 +0000

    change to hashrouter for static server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './components/App';
 
 import styles from './styles/Reset.scss';
 
-ReactDOM.render(<BrowserRouter basename='/react-portfolio'><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter basename='/react-portfolio'><App /></HashRouter>, document.getElementById('root'));
